438964fcfc6130c1e9a897b124175180841d8f93
conrad85E <marvin777@bk.ru>
2025-12-07 06:57:36 +0500

    Add final formatting to D1.py

--- AoC_2025/D1.py
+++ AoC_2025/D1.py
@@ -1,6 +1,6 @@
 def rotate(direction: str, steps: int, start: int) -> tuple[int, int]:
     '''
-    Performs one turn and returns the end position and number of times 0 digit was passed.
+    Performs one rotation of a dial and returns end position and number of times 0 digit was passed.
     
     :param direction: Direction of rotation ("L" or "R").
     :type direction: str
@@ -8,7 +8,8 @@ def rotate(direction: str, steps: int, start: int) -> tuple[int, int]:
     :type steps: int
     :param start: Starting position before the rotation.
     :type start: int
-    :return: Tuple of a [new position] and [number of passes of 0 mark] during the rotation.
+    
+    :return: Tuple of the [new position] and [number of passes of 0 digit during the rotation].
     :rtype: tuple[int, int]
     '''
     dial_length = 100       # number of digits on the dial
@@ -35,20 +36,24 @@ def rotate(direction: str, steps: int, start: int) -> tuple[int, int]:
     
     return end_position, zeros_passed
 
-curr_position = 50
-zeros_passed_total = 0
+def main():
+    curr_position = 50
+    zeros_passed_total = 0
 
-with open("D1_input.txt", "r") as file:
-    for line in file:
-        turn = line.strip()
+    with open("D1_input.txt", "r") as file:
+        for line in file:
+            turn = line.strip()
 
-        direction = turn[0]     # "L" or "R"
-        steps = int(turn[1:])   # amount of steps
+            direction = turn[0]     # "L" or "R"
+            steps = int(turn[1:])   # amount of steps
 
-        if direction not in "LR":
-            raise ValueError("The 'direction' must be 'L' or 'R'")
-        next_position, zeros_passed = rotate(direction, steps, curr_position)
-        zeros_passed_total += zeros_passed
-        curr_position = next_position
+            if direction not in "LR":
+                raise ValueError("The 'direction' must be 'L' or 'R'")
+            next_position, zeros_passed = rotate(direction, steps, curr_position)
+            zeros_passed_total += zeros_passed
+            curr_position = next_position
 
-print(zeros_passed_total)
+    print(zeros_passed_total)
+
+if __name__ == "__main__":
+    main()
