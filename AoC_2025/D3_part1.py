eee6894295adc5323b34b3eefa052db37a53f79a
conrad85E <marvin777@bk.ru>
2025-12-09 08:19:45 +0500

    Add solution to AoC2025 Day3 Part1 & input data file: D3_part1.py|D3_input.txt

---
+++ AoC_2025/D3_part1.py
@@ -0,0 +1,27 @@
+def max_joltage(bank: str) -> int:
+    first_battery = float("-inf")
+    second_battery = float("-inf")
+
+    for i in range(len(bank)):
+        curr_battery = int(bank[i])
+        if curr_battery > first_battery and i != (len(bank) - 1):
+            first_battery = curr_battery
+            second_battery = float("-inf")
+        else:
+            if curr_battery > second_battery:
+                second_battery = curr_battery
+    
+    return first_battery * 10 + second_battery
+    
+def main():
+    joltage_sum = 0
+    with open("D3_input.txt", "r") as f:
+        for line in f:
+            bank = line.strip()
+            bank_joltage = max_joltage(bank)
+            joltage_sum += bank_joltage
+    print(joltage_sum)
+
+if __name__ == "__main__":
+    main()
+    # print(max_joltage("123456"))
